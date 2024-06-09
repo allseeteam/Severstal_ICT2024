098400608dc982314937bacd0ebdd944e7bd5ac2
svkov <svkov42@yandex.ru>
2024-06-09 17:35:41 +0300

    integrate search to api

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+import pickle
+
+search_engine = pickle.load(open('search.pkl', 'rb'))
+
+
+def search(request):
+    query = request.GET['q']
+    result = search_engine.search(query)
+    return JsonResponse(result, safe=False)

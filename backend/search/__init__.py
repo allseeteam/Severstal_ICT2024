d58bf5b728f53daf45eeed3b387d778e4f369714
svkov <svkov42@yandex.ru>
2024-06-14 17:25:47 +0300

    add yandex search

--- backend/search/__init__.py
+++ backend/search/__init__.py
@@ -3,3 +3,4 @@ from search.search import SearchEngine  # noqa
 from search.entity import *  # noqa
 from search.rank import *  # noqa
 from search.query import enrich_query  # noqa
+from search.ya_search import ya_search  # noqa

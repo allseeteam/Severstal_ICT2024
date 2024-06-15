ad15b9a284ac3615953a08f4cc718c594eeed3de
svkov <svkov42@yandex.ru>
2024-06-15 18:37:53 +0300

    refactor

--- backend/accounts/management/commands/generate_reports.py
+++ backend/accounts/management/commands/generate_reports.py
@@ -1,13 +1,6 @@
 from django.core.management.base import BaseCommand
 from accounts import models
-
-import plotly
-import plotly.graph_objs as go
-from extract import get_table_fig, preprocess_entity
-from export import save_excel_report, save_pdf_report, save_word_report
-
-from django.forms import model_to_dict
-import markdown
+from export import save_excel_report, save_pdf_report, save_word_report, preprocess_blocks
 
 
 class Command(BaseCommand):
@@ -21,50 +14,7 @@ class Command(BaseCommand):
             print('Нет блоков по такому id')
             return
         blocks = [blocks[0], blocks[0]]  # для теста. в проде закомментить.
-        new_blocks, tables = save_blocks_to_jpg(blocks)
+        new_blocks, tables = preprocess_blocks(blocks)
         save_pdf_report(new_blocks, 'output_report.pdf')
         save_word_report(new_blocks, 'output_report.docx')
         save_excel_report(tables, 'output_report.xlsx')
-
-
-def save_blocks_to_jpg(blocks):
-    """
-    возвращает два объекта
-    1. список словарей готовых данных блоков для рендера в ворд и пдф
-    2. список датафреймов (которые лежат внутри блоков) - для экселя
-    """
-    tables = []
-    new_blocks = []
-    for i, block in enumerate(blocks):
-        data = block.data
-        source = data.page.url
-        type = block.type
-        if type == 'plotly':
-            representation = block.representation
-            entity = model_to_dict(data)
-            entity['frame'] = entity['data']
-            entity['meta'] = entity['meta_data'].get('title', '')
-            entity = preprocess_entity(entity)
-            table = get_table_fig(entity)
-            fig = go.Figure(representation)
-            filename = f'block_{block.id}.jpg'
-            filename_table = f'block_{block.id}_table.jpg'
-            plotly.io.write_image(fig, filename, format='jpg')
-            plotly.io.write_image(table, filename_table, format='jpg')
-            tables.append(entity['frame'])
-            new_blocks.append({
-                'path_fig': filename,
-                'path_table': filename_table,
-                'type': 'plotly',
-                'source': source,
-                'position': i,
-            })
-        else:
-            new_blocks.append({
-                'text': data.data,
-                'html_text': markdown.markdown(data.data),
-                'type': 'text',
-                'source': source,
-                'position': i,
-            })
-    return new_blocks, tables
